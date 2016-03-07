ef1497ef571be96df5275829d471381f03a54f23
qqibrow <qqibrow@gmail.com>
2016-03-06 18:42:46 -0800

    try to implement one that scrape the whole website

--- KbbPriceSpider.py
+++ KbbPriceSpider.py
@@ -1,9 +1,20 @@
-import scrapy
 import re
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
-class KbbPriceSpider(scrapy.Spider):
+class KbbPriceSpider(CrawlSpider):
     name = "kbb price"
+    allowed_domains = ['kbb.com']
     start_urls = ['http://www.kbb.com/bmw/3-series/2015/320i-sedan-4d/?vehicleid=402899&intent=buy-used&category=sedan&mileage=19387&condition=excellent&pricetype=private-party']
+    #start_urls = ['http://www.kbb.com/bmw/3-series/2015/']
+    #rules = (
+         #Rule(LinkExtractor(allow=('bmw'))),
+    #)
+
+    rules = [Rule(SgmlLinkExtractor(allow=()), callback='parse_item', follow=True)]
+
+    def parse_item(self,response):
+        self.log('A response from %s just arrived!' % response.url)
 
     def parse(self, response):
         p = re.compile(ur'(?:\"values\": )({(?:\s|.)*?)(?:,\s+\"timAmount\")')
