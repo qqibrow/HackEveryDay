9f6f52d35116ecc83cae0d5afb4e254f6fa38c6e
qqibrow <qqibrow@gmail.com>
2016-03-10 23:36:42 -0800

    add kbb price spider

--- KbbPriceSpider.py
+++ KbbPriceSpider.py
@@ -1,17 +1,32 @@
 import re
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.linkextractors import LinkExtractor
+#from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 class KbbPriceSpider(CrawlSpider):
     name = "kbb price"
     allowed_domains = ['kbb.com']
-    start_urls = ['http://www.kbb.com/bmw/3-series/2015/320i-sedan-4d/?vehicleid=402899&intent=buy-used&category=sedan&mileage=19387&condition=excellent&pricetype=private-party']
+    #start_urls = ['http://www.kbb.com/bmw/3-series/2015/320i-sedan-4d/?vehicleid=402899&intent=buy-used&category=sedan&mileage=19387&condition=excellent&pricetype=private-party']
     #start_urls = ['http://www.kbb.com/bmw/3-series/2015/']
+    start_urls = ['http://www.kbb.com/bmw/3-series/2012/categories/?intent=buy-used']
     #rules = (
          #Rule(LinkExtractor(allow=('bmw'))),
     #)
 
-    rules = [Rule(SgmlLinkExtractor(allow=()), callback='parse_item', follow=True)]
+    rules = [
+             Rule(LinkExtractor(deny=('cars-for-sale')), follow=False),
+             Rule(LinkExtractor(allow=('bodystyle')), callback='parse_item', follow=True),
+             Rule(LinkExtractor(allow=('vehicleid')), callback='parse_item', follow=True),
+             Rule(LinkExtractor(allow=('bmw')), callback='parse_item', follow=True),
+             Rule(LinkExtractor(allow=('valuetype')), callback='parse_item', follow=True),
+             Rule(LinkExtractor(allow=('pricetype')), callback='parse_item', follow=False),
+             Rule(LinkExtractor(allow=('pricetype')), callback='parse_item', follow=False)]
 
-    def parse_item(self,response):
-        self.log('A response from %s just arrived!' % response.url)
+    def parse_item(self, response):
+        p = re.compile(ur'(?:\"values\": )({(?:\s|.)*?)(?:,\s+\"timAmount\")')
+        matched = p.search(response.body)
+        if matched:
+          yield {
+              'url': response.url,
+              'prices': matched.group(1)
+          }
